ced847f2e5878d53cf5bd8d1b7bef83f6ec98853
Jennifer Christie Christie <jennifer.christie1@gmail.com>
2016-12-02 13:39:23 -0500

    finished2

--- app.js
+++ app.js
@@ -231,7 +231,7 @@ app.delete('/deletePost', function (req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('listening on port 3000!');
 });
 
